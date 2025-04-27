65e94aa7356c97b578e097532a34d27ca2c10405
Nereaaaa <100692926+Nereaaaa@users.noreply.github.com>
2025-04-27 16:05:45 +0200

    Empty Structure
    Empty structure of the three Spring Boot proyects: Api, Disk and Instance

---
+++ disk/src/test/java/es/codeurjc/disk/DiskApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.disk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ instance/src/test/java/es/codeurjc/instance/InstanceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.instance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
